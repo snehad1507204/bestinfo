89d793d4a4408c50fb33884eb695f7982db0a21a
AbhayG9119 <140013541+AbhayG9119@users.noreply.github.com>
2025-09-01 08:49:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import BLogs from './pages/BLogs';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/BLogs" element={<BLogs />} />
         <Route path="/About" element={<About />} />
-        <Route path="/GreenWasty" element={<GreenWasty />} />
         <Route path="/Contact" element={<Contact />} />
-      </Routes> */}
+      </Routes>
+      <Footer />
     </Router>
   );
 }
